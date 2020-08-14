421d64eebe0b4d030447e282e76aae4f989e32c6
“janarthanan” <janarthanandevs@gmail.com>
2020-08-14 21:08:51 +0530

    commented s3 endpoint

--- loadtest.py
+++ loadtest.py
@@ -5,7 +5,7 @@ warehouseLocation = 's3a://s3-phc-poc-02-sample-etl/'
 spark = SparkSession.builder.appName("Test").config("spark.sql.warehouse.dir", warehouseLocation).getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("INFO")
-sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 's3.amazonaws.com')
+#sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 's3.amazonaws.com')
 #data_df = spark.read.csv("s3a://s3-phc-poc-02-sample-etl/2020/08/*.gz")
 data_df = spark.read.csv(sys.argv[1])
 print('10 rows to display:')
